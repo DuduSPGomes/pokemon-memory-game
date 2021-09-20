9a147329ab6e39af1cf0ae7df99fa5e4e106f08e
Cadu <dudu.spgomes@gmail.com>
2021-09-20 00:18:11 -0300

    feat: untap only 2 cards

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,40 +1,55 @@
 import { pokemonTypesBase } from '../../data/constants/pokemon-types.js'
 
-/** 
-  * @pokemon Any pokemon
+let clickTimes = 0
+let clickedCards = []
+let imgsOfclickedCards = []
+let pokemonOfclickedCards = []
+
+const untapCard = (card, img, pokemon) => {
+  card?.classList?.remove('tappedCard')
+  card?.classList?.add('untappedCard')
+  img.src = pokemon.artwork
+  img?.classList?.add('artwork')
+
+  handlePokemonTypes('add', pokemon, card, pokemonTypesBase)
+
+  img.classList.remove('d-none')
+}
+
+const tapCard = (card, img, pokemon) => {
+  card?.classList?.remove('untappedCard')
+  card?.classList?.add('tappedCard')
+  img?.classList?.add('d-none')
+
+  handlePokemonTypes('remove', pokemon, card, pokemonTypesBase)
+}
+
+const handlePokemonTypes = (handle, pokemon, card, pokemonTB) => {
+  const pokemonType = pokemon?.types[0]?.type?.name
+
+  pokemonTB?.forEach((ptb) => {
+    if (ptb === pokemonType && handle === 'add') {
+      card?.classList?.add(`bg-color-${pokemon?.types[0]?.type?.name}`)
+    }
+    if (ptb === pokemonType && handle === 'remove') {
+      card?.classList?.remove(`bg-color-${pokemon?.types[0]?.type?.name}`)
+    }
+  })
+}
+
+const clearTapCardConstants = () => {
+  clickedCards = []
+  imgsOfclickedCards = []
+  pokemonOfclickedCards = []
+  clickTimes = 0
+}
+
+
+/**
+  * Returns a pokemon Card
+  * @pokemon Pokemon object
 */
-
 export default (pokemon) => {
-  const untapCard = (card, img, pokemon) => {
-    card.classList.remove('tappedCard')
-    card.classList.add('untappedCard')
-    img.src = pokemon.artwork
-    img.classList.add('artwork')
-
-    const pokemonType = pokemon?.types[0]?.type?.name
-
-    pokemonTypesBase.forEach((ptb) => {
-      if (ptb === pokemonType) {
-        card.classList.add(`bg-color-${pokemon?.types[0]?.type?.name}`)
-      }
-    })
-
-    img.classList.remove('d-none')
-  }
-
-  const tapCard = (card, img) => {
-    card.classList.remove('untappedCard')
-    card.classList.add('tappedCard')
-    img.classList.add('d-none')
-
-    const pokemonType = pokemon?.types[0]?.type?.name
-
-    pokemonTypesBase.forEach((ptb) => {
-      if (ptb === pokemonType) {
-        card.classList.remove(`bg-color-${pokemon?.types[0]?.type?.name}`)
-      }
-    })
-  }
 
   const Card = document.createElement('li')
   const img = document.createElement('img')
@@ -43,11 +58,31 @@ export default (pokemon) => {
 
   Card.classList.add('card', 'tappedCard')
 
-  Card?.addEventListener('click', () => {
+  Card?.addEventListener('click', (e) => {
     if (Card.classList.contains('tappedCard')) {
-      untapCard(Card, img, pokemon)
-    } else {
-      tapCard(Card, img)
+      if (clickTimes < 2) {
+        console.log(clickTimes)
+        untapCard(Card, img, pokemon)
+        clickTimes += 1
+        clickedCards.push(Card)
+        imgsOfclickedCards.push(img)
+        pokemonOfclickedCards.push(pokemon)
+      }
+    }
+    if (Card.classList.contains('untappedCard')) {
+      if (clickTimes === 2) {
+        if (pokemonOfclickedCards[0].name !== pokemonOfclickedCards[1].name) {
+          setTimeout(function () {
+            tapCard(clickedCards[0], imgsOfclickedCards[0], pokemonOfclickedCards[0])
+            tapCard(clickedCards[1], imgsOfclickedCards[1], pokemonOfclickedCards[1])
+            clearTapCardConstants()
+          }, 3000)
+        } else {
+          setTimeout(function () {
+            clearTapCardConstants()
+          }, 3000)
+        }
+      }
     }
   })
 
